774add571bc575c469047fe08ad6e580828a9495
hpgpanda <fishing_cat@139.com>
2024-09-29 16:56:43 +0800

    new

---
+++ hms_robot/localization_init/src/color_points/src/filter.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/voxel_grid.h>
+#include "sensor_msgs/PointCloud2.h"
+
+int main()
+{
+    // 加载点云数据
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+    if (pcl::io::loadPCDFile<pcl::PointXYZI>("/home/gk/外高桥煤厂/2chang.pcd", *cloud) == -1)
+    {
+        std::cerr << "Failed to load input point cloud!" << std::endl;
+        return -1;
+    }
+
+    // 创建体素滤波对象
+    pcl::VoxelGrid<pcl::PointXYZI> voxelgrid;
+    voxelgrid.setInputCloud(cloud);
+    voxelgrid.setLeafSize(0.5f, 0.5f, 0.5f);  // 设置体素网格的尺寸
+
+    // 执行体素滤波
+    pcl::PointCloud<pcl::PointXYZI>::Ptr filteredCloud(new pcl::PointCloud<pcl::PointXYZI>);
+    voxelgrid.filter(*filteredCloud);
+
+
+    // 保存滤波后的点云数据
+    pcl::io::savePCDFileBinary("/home/gk/外高桥煤厂/B_filter.pcd", *filteredCloud);
+    std::cout << "Filtered point cloud saved." << std::endl;
+
+    return 0;
+}
